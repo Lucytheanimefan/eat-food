26d2c59969b44685d424615d4a5b4c3ba2d83abc
amanocha <aninda.manocha@gmail.com>
2016-11-19 13:54:14 -0500

    added nutrient calculator

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 
 nix = Nutritionix(app_id="7f770e5d", api_key="dae4065c600b6b161789a27471167ccd") #make these env variables later
 
-
-
-
-
 @app.route("/")
 def hello():
 	#print nix.search('pizza').json()
