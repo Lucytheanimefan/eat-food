8afcfbd8fdcb3041bca30f1e4e205ce22b79a60d
amanocha <aninda.manocha@gmail.com>
2016-11-19 21:19:28 -0500

    added open account functionality

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import app
 import server
 
 db = server.get_db()
@@ -9,3 +10,9 @@ def create_account(username, password):
         db.users.insert({"username": username, "password": password})
         return "Success! Your account has been created."
 
+def open_account(username, password):
+    if (db.users.find({"username": "amanocha"})[0]["password"] != password):
+        return "Invalid username/password."
+    else:
+        app.set_username(username)
+        return "Success!"
