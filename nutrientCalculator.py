18836337d7961c224f37a23ea55bba7669b930df
Yumin Zhang <yz263@duke.edu>
2016-11-19 22:45:42 -0500

    change parameters

--- nutrientCalculator.py
+++ nutrientCalculator.py
@@ -96,4 +96,4 @@ def get_info(username, gender_string, age_string, height_feet, height_inches, we
     )
 
 print(login.create_account("amanocha", "password"))
-get_info("amanocha", "female", 19, 5, 2, 120, "moderate")
+get_info("amanocha", "female", 19, 5, 2, 120, "moderate", [])
